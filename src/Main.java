39b58cedf53ecedc16f20f972e7dc1625539e518
Muneef <rai.muneef@yahoo.com>
2019-08-08 06:55:27 -0400

    home work

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=20;
+		
+		while(x<30);
+		System.out.println("value of x:" +x);
+		x++;
+		System.out.println("\n");
+
+	}
+
+}
